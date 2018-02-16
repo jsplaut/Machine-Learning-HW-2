6870bc870dffa6be906d84d41989917c82aac044
James S. Plaut II <jsplaut@jamess-mbp.wireless.davidson.edu>
2018-02-15 21:52:45 -0500

    final code before draft submission

--- MNIST.py
+++ MNIST.py
@@ -5,7 +5,7 @@ from read_mnist import load_data, pretty_print
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn import model_selection, neighbors, metrics
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict, LeaveOneOut
 
 def logisticRegression1(xd):
     train_set, test_set = load_data()
@@ -19,7 +19,6 @@ def logisticRegression1(xd):
     # xTrain = scaler.fit_transform(xTrain)
     # xTest = scaler.transform(xTest)
 
-    train_samples = 5000
     clf = LogisticRegression(C=1.0,
                             multi_class='multinomial',
                             penalty='l1', solver='saga', tol=0.1)
@@ -42,7 +41,7 @@ def logisticRegression1(xd):
     for i in range(10):
         l1_plot = plt.subplot(2, 5, i + 1)
         l1_plot.imshow(coef[i].reshape(28, 28), interpolation='nearest',
-                       cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
+                    cmap=plt.cm.RdBu, vmin=-scale, vmax=scale)
         l1_plot.set_xticks(())
         l1_plot.set_yticks(())
         l1_plot.set_xlabel('Class %i' % i)
@@ -118,10 +117,30 @@ def knn(xD):
         print("\n Accuracy Score for %d neighbors: %.4f \n" % (n, score))
         print(metrics.classification_report(yTest,pred, digits = 4))
 
+def crossValidationLOO(xd):
+    train_set, test_set = load_data()
+
+    yTrain = train_set[1]
+    xTrain = train_set[0]
+    yTest = test_set[1]
+    xTest = test_set[0]
+
+    kfolds = model_selection.KFold(n_splits = 10, shuffle=True)
+
+    clf = neighbors.KNeighborsClassifier(n_neighbors=3)
+
+    # score = cross_val_score(clf, xTrain, yTrain, cv=kfolds, scoring='accuracy')
+    # print("10-fold cross validation average accuracy: %.3f" % (score.mean()))
+
+    pred = cross_val_predict(clf, xTest, yTest, cv=kfolds)
+    print("Accuracy of prediction model: %.4f \n" % metrics.r2_score(yTest, pred))
+    print(metrics.classification_report(yTest, pred, digits = 3))
+
 
 
 #logisticRegression1(1)
 #baseLines(1)
 #logiCrossValidation()
 #knnDistance(1)
-knn(1)
+#knn(1)
+crossValidationLOO(1)
